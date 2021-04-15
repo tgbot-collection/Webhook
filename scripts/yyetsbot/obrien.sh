64b84167a50abcbbe233860d4cbe4a3c03a1f046
BennyThink <benny.think@gmail.com>
2021-04-15 22:03:48 +0800

    fix obrien bug

--- scripts/yyetsbot/obrien.sh
+++ scripts/yyetsbot/obrien.sh
@@ -2,5 +2,5 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
 docker pull bennythink/yyetsbot
-docker-compose -f/root/BotsRunner/docker-compose.yml up -d yyets
+docker-compose -f /root/BotsRunner/docker-compose.yml up -d yyets
 #docker system prune -a --volumes -f
